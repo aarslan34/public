d35cf799833f1c850e1489f4a9bb124f915fc08a
Ahmet Arslan <36181274+aarslan34@users.noreply.github.com>
2021-08-19 11:07:50 -0500

    Create EmployeeServiceImpl.java

---
+++ EmployeeServiceImpl.java
@@ -0,0 +1,31 @@
+package com.tcs.emplmngt.service;
+
+import com.tcs.emplmngt.model.Employee;
+
+public class EmployeeServiceImpl implements EmployeeService{
+
+    @Override
+    public String addEmployee(Employee employee) {
+        return null;
+    }
+
+    @Override
+    public String updateEmployee(String empId, Employee employee) {
+        return null;
+    }
+
+    @Override
+    public String deleteEmployeeById(String id) {
+        return null;
+    }
+
+    @Override
+    public Employee getEmployeeById(String id) {
+        return null;
+    }
+
+    @Override
+    public Employee[] getEmployees() {
+        return new Employee[0];
+    }
+}
